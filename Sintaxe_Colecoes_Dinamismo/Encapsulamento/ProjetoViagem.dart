414f1e025ecb05f3d24fcabe4018f3796feaa64e
GabrielJBrito <gabriel.jbrito2@gmail.com>
2023-10-03 19:16:04 -0300

    Construindo o Encapsulamento

--- Sintaxe_Colecoes_Dinamismo/Encapsulamento/ProjetoViagem.dart
+++ Sintaxe_Colecoes_Dinamismo/Encapsulamento/ProjetoViagem.dart
@@ -1,6 +1,17 @@
+import 'Transporte.dart';
+import 'Viagem.dart';
+
 main(){
 
  
+Viagem viagemMaio = Viagem(locomocao: Transporte.aviao);
+print(viagemMaio.consultarTotalLocaisVisitados); //chamando o método get (da propriedade encapsulada '_totalLocaisVisitados')
 
+viagemMaio.visitar("Museu"); //Adicionando um local visitado
+print(viagemMaio.consultarTotalLocaisVisitados); //Chamando o método para verificar quantos locais foram visitados.
 
+viagemMaio.alterarLocaisVisitados = 10; //Utilizando o método de atualizar a propriedade privada. Utiliza-se se o = + novo valor quando se trata de um 'set'
+                                        //diferente dos métodos comuns que passamos a informação via parâmetro entre parenteses.
+
+print(viagemMaio.consultarTotalLocaisVisitados); //Verificando o novo total de lugares visitados.
 }
--- Sintaxe_Colecoes_Dinamismo/Encapsulamento/Viagem.dart
+++ Sintaxe_Colecoes_Dinamismo/Encapsulamento/Viagem.dart
@@ -9,6 +9,8 @@ class Viagem{
   Transporte locomocao;
   Set<String> registrosVisitados = <String>{};
   Map<String, dynamic> RegistrarPrecos = {}; 
+  int _totalLocaisVisitados =0; // o '_' no inicicio do nome da propriedade priva ela, ou seja, a encapsula. Isso significa que no código principal não será possível
+                                // visualiza-la, impedindo que qualquer valor indesejado seja atribuido a ela. Para visualiza-la utilizamos o GETTER.
 
   //Construtor Viagem
   Viagem({required this.locomocao}); //Há somente locomocao, pois os demais já se inicializam de alguma forma, vazios ou em 0.
@@ -46,6 +48,7 @@ class Viagem{
   //Método para adicionar um local visitado no Set.
   void visitar(String localVsita){
   registrosVisitados.add(localVsita);
+  _totalLocaisVisitados += 1; //Incrementado mais um lugar visitado, para cada vez que houver um cadastro de nova visita.
 
 }
 
@@ -54,6 +57,26 @@ class Viagem{
   RegistrarPrecos[local] = preco;
 
 }
+
+
+  //-------GETTER--------
+  //PARA PEGAR UMA INFORMAÇÃO PRIVADA, OU ENCAPSULADA, UTILIZAMOS O GET.
+  int get consultarTotalLocaisVisitados{ // Essa é a sintaxe do get, não é necessário '()' pois, ele n precisa de parâmetros, ele apenas pega uma informação.
+
+    return _totalLocaisVisitados;        //Dessa maneira, controlamos a informação que quem utilizará a classe terá. Podemos retornar qualquer valor a ela se quisermos.
+                                        //escondendo assim, a informação do valor real 0
+  }
+
+  //-------SETTER--------
+  //PARA ATUALIZAR UMA INFORMAÇÃO PRIVADA, OU ENCAPSULADA, UTILIZAMOS O SET
+  void set alterarLocaisVisitados(int novaQtd){ //Essa é a sintaxe do set, diferente do get, como vamos atualizar uma informação, ele espera receber um parâmetro
+                                                //ou seja, o que iremos atualizar.
+    
+    _totalLocaisVisitados = novaQtd; //Substituindo uma nova quantidade de um local visitado.
+                                     //Se tratando de um tratamento de propriedade privada, também temos o controle de como essa nova infomração poderá ser passado
+                                     //Por exemplo, usando um if dizendo que só pode ser informado números menores do que 10. 
+
+  }
 
 }
 
