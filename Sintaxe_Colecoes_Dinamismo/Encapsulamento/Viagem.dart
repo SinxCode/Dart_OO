b827b2e39d9b9497fc916a4bb9a21f8dcb67634e
Sinx <gabriel.jbrito2@gmail.com>
2023-10-03 17:16:10 -0300

    Construção Classe Viagem

--- Sintaxe_Colecoes_Dinamismo/Encapsulamento/Viagem.dart
+++ Sintaxe_Colecoes_Dinamismo/Encapsulamento/Viagem.dart
@@ -3,24 +3,26 @@ import '../Enum.dart';
 
 class Viagem{
 
-  //static serve para conseguirmos acessar algum dado diretamente na estrutura da classe, sem necessitar de instancia (construtor ou método).
+  //Propriedades Classe Viagem
   static String codigoTrabalho = 'kjswedgkj';
   double dinheiro = 0;
   Transporte locomocao;
   Set<String> registrosVisitados = <String>{};
   Map<String, dynamic> RegistrarPrecos = {}; 
 
-  Viagem({required this.locomocao});
+  //Construtor Viagem
+  Viagem({required this.locomocao}); //Há somente locomocao, pois os demais já se inicializam de alguma forma, vazios ou em 0.
 
+  //Método para printar o código de trabalho
   printCodigo(){
     print(codigoTrabalho);
   }
 
-  
-void escolherMeioTransporte(Transporte locomocao){ 
+  //Método para Escolher o meio de transporte
+  void escolherMeioTransporte(Transporte locomocao){ 
 
-switch(locomocao){ 
-  case Transporte.carro:
+  switch(locomocao){ 
+    case Transporte.carro:
     print('Vou de carro para aventura.');
     break;
 
@@ -33,15 +35,23 @@ switch(locomocao){
     break;
 
     default:
-    print('Estou indo para aventura');
+    print('Estou indo para aventura de qualquer maneira.');
     break;
 
 }
 
 }
 
-void visitar(String localVsita){
-registrosVisitados.add(localVsita);
+
+  //Método para adicionar um local visitado no Set.
+  void visitar(String localVsita){
+  registrosVisitados.add(localVsita);
+
+}
+
+  //Método registrar o custo da visita no Map.
+  void registrarPrecoVisita(String local, dynamic preco){
+  RegistrarPrecos[local] = preco;
 
 }
 
