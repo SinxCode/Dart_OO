4ca27a8f23cf2ef1c31841a494feecb2633346a6
Sinx <gabriel.jbrito2@gmail.com>
2023-10-04 08:46:00 -0300

    Código Principal

---
+++ Erros_Excecoes_PilhadeExecucao/ProjetoBanco.dart
@@ -0,0 +1,20 @@
+import 'controllers/bank_controller.dart';
+import 'models/account.dart';
+
+void main(){
+
+  //Criandoo banco
+  BankController bankController = BankController();
+
+  //Adicionando contas
+  bankController.addAccount(id: 'Sinx', account: Account(name: 'Sinx Stars', balance: 2000, isAuthenticated: true));
+  bankController.addAccount(id: 'Honey', account: Account(name: 'Honey Bunny', balance: 1000, isAuthenticated: true));
+
+  //Fazendo transferência
+  bool result = bankController.makeTransfer(idSender: 'Sinx', idReceiver: 'Honey', ammount: 100);
+
+  //observando resultado
+  print(result);
+
+
+}
